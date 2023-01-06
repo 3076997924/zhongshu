36b2671b6622eedb22e4a10afa4dae4d783b5c4d
高攀 <3076997924@qq.com>
2023-01-06 11:12:05 +0800

    first

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$(function(){
+
+
+
+
+    
+
+    $('.consult-rt-human').on('click',function(){
+        window.location.href = 'human.html'
+    })
+    $('.consult-rt-culture').on('click',function(){
+        window.location.href = 'culture.html'
+    })
+})
